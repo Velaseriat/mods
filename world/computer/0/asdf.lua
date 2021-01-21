5fbf3707e1d8c4d2c1bb539ffdb8affe9a3efef6
Velaseriat <velaseriat@outlook.com>
2021-01-20 17:50:45 -0700

    backup

---
+++ world/computer/0/asdf.lua
@@ -0,0 +1,4 @@
+while turtle.detect() do
+    turtle.dig()
+    sleep(1.1)
+end
---
+++ world/computer/32/tower.lua
@@ -0,0 +1,15 @@
+local args = {...}
+local height = tonumber(args[1]) or 5
+local width = tonumber(args[2]) or 3
+
+for i = 1, height do
+    for x = 1, 4 do
+        for y = 1, width-1 do
+            turtle.forward()
+            turtle.placeDown()
+        end
+        turtle.turnRight()
+    end
+    turtle.up()
+end
+
